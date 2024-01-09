d71502566e2e0a5f124453867e3cd20c7b02efd0
Hayate Nakano <nakanodesu@gmail.com>
2024-01-10 02:41:02 +0900

    use clap

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 mod vault;
 
 use anyhow::Result;
-use rpassword;
 use std::path::Path;
 
 fn main() -> Result<()> {
-    let vault_root = Path::new(env!("CARGO_MANIFEST_DIR")).join("sample_vault");
-    let vault_tgt_root = Path::new(env!("CARGO_MANIFEST_DIR")).join("sample_vault_tgt");
+    let m = clap::Command::new("cryptomator-extractor")
+        .arg(clap::Arg::new("vault").index(1).required(true))
+        .arg(clap::Arg::new("target").index(2).required(true))
+        .get_matches();
+    let vault_root = Path::new(m.get_one::<String>("vault").unwrap());
+    let vault_tgt_root = Path::new(m.get_one::<String>("target").unwrap());
     let passphrase = rpassword::prompt_password("Your passphrase: ")?;
     vault::Vault::new(&vault_root, &passphrase)?.decrypt_dir_from_root(&vault_tgt_root)
 }
